2cde26684688e77b7c27a5c4e29b371f56d2b401
Prakadeeswaran <35056431+Prakadeeswaran@users.noreply.github.com>
2018-12-31 20:47:25 +0530

    Two way binding
    Two way binding completed..

--- src/index.js
+++ src/index.js
@@ -3,13 +3,49 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: "Prakadees"
+  };
+
+  handleChangeState = name => {
+    this.setState({
+      name: name
+    });
+  };
+  handleChange = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <button
+          onClick={() =>
+            this.handleChangeState("This is awsome prakadees by ANNN.")
+          }
+        >
+          Change state using ann function
+        </button>
+        <button
+          onClick={this.handleChangeState.bind(
+            this,
+            "This is awsome prakadees by BIND."
+          )}
+        >
+          Change state using bind
+        </button>
+
+        <h1>{this.state.name}</h1>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
